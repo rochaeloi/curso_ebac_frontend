bcab59ac69e1a38fa85b575eebd6cd1f899d27df
eloi rocha <eloirocha2010@gmail.com>
2024-09-10 21:06:44 -0300

    commit inicial

---
+++ exercicio.js
@@ -0,0 +1,11 @@
+"use strict";
+function multiplicar(a, b) {
+    return a * b;
+}
+function saudar(nome) {
+    return "Olá " + nome;
+}
+const resultadoMultiplicacao = multiplicar(5, 3);
+console.log(resultadoMultiplicacao);
+const saudacao = saudar("Carlos");
+console.log(saudacao);
---
+++ exercicio.ts
@@ -0,0 +1,13 @@
+function multiplicar(a: number, b: number): number {
+    return a * b;
+}
+
+function saudar(nome: string): string {
+    return "Olá " + nome;
+}
+
+const resultadoMultiplicacao = multiplicar(5, 3);
+console.log(resultadoMultiplicacao); 
+
+const saudacao = saudar("Carlos");
+console.log(saudacao);
